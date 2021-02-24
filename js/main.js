b5b84fccf0bfca80989ecc48bdd7c6cbe9f50b91
saulpark <aks_@naver.com>
2021-02-25 04:11:21 +0900

    ajax 통신 가능, 영역별 문제정보 동적으로 표시 가능, 달력에 select box 연동

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+
+let visual_problems = ['같은그림찾기(하)', '같은도형찾기', '반쪽그림따라그리기', '특정글자찾기', '틀린그림찾기', '특정인물찾기', '같은글미잇기', '반쪽그림잇기', '이름찾아적기', '같은그림찾기(중)'];
+let concentration_problems = ['미로찾기', '사다리타기', '색칠하기', '기념일 색칠하기', '같은 숫자 연결하기', '화투 색칠하기', '글자색깔맞추기', '선따라 그리기', '따라그리기(중)', '따라그리기(하)'];
+let thinking_problems = ['물건개수 맞추기', '4x4스도쿠', '개수맞추기', '범주찾기', '주제와다른것찾기', '시장물건 계산하기', '주사위 계산하기', '열매개수세기', '숫자 나열하기'];
+let language_problems = ['빈칸채우기', '초성맞추기', '동물속담맞추기', '끝말잇기', '단어와 그림맞추기', '단어만들기', '명언따라쓰고읽기', '문장따라말하기'];
+let remembrance_problems = ['기념일 회상하기', '요리재료 맞추기', '식재료 구분하기', '옛물건 회상하기', '정보 기억하기', '음식 회상하기', '사물회상하기', '계절 회상하기', '옛놀이 회상하기'];
+let life_problems = ['시간 맞추기', '야외활동 함께하기', '실내활동 함께하기', '집안일 함께하기', '냉장고 식재료 찾기', '표정 맞추기', '실내운동 함께하기', '물건의 용도 알기'];
+
+
 // ajax communication
 const person_add = document.querySelector('.person-control__add');
 person_add.addEventListener('click', (event) => {
@@ -5,13 +14,14 @@ person_add.addEventListener('click', (event) => {
     let data = "fucking good";
 
     $.ajax({
-		type : 'POST',
-		url : '/center-learning',
+		type : 'GET',
+		url : '/center-learnings',
 		data : JSON.stringify(data),
 		contentType : 'application/json; charset=utf-8',
 		dataType : 'json'
 	}).done(function(r) {
-		if (r.statusCode == 200) {
+		if (r.status == "OK") {
+            console.log(r.data[0].id);
 			alert('통신 성공');
 		} else {
 			alert('통신 실패');
@@ -19,7 +29,6 @@ person_add.addEventListener('click', (event) => {
 	}).fail(function(r) {
 		alert('서버 오류');
 	});
-
 });
 
 
@@ -75,6 +84,7 @@ function buildCalendar(){//현재 달 달력 만들기
         row = tbCalendar.insertRow();
         //테이블에 새로운 열 삽입//즉, 초기화
         var cnt = 0;// count, 셀의 갯수를 세어주는 역할
+        var emptyCnt = 0; // 1일 전에 공백 개수 세기
     // 1일이 시작되는 칸을 맞추어 줌
 
         cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
@@ -87,6 +97,8 @@ function buildCalendar(){//현재 달 달력 만들기
             cell.classList.add('calendar__number');
             cnt = cnt + 1;//열의 갯수를 계속 다음으로 위치하게 해주는 역할
         }
+
+        emptyCnt = cnt;
 
     /*달력 출력*/
         for (i=1; i<=lastDate.getDate(); i++) { 
@@ -109,22 +121,77 @@ function buildCalendar(){//현재 달 달력 만들기
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "인지활동 (1)";
                 cell.classList.add('calendar__index');
+                let tempEmptyCnt = emptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
+                    console.log("1:"+tempEmptyCnt+" , " + tbCalendar.rows.length);
+
+                    if(tempEmptyCnt==0 || tbCalendar.rows.length >= 7){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                    }else {
+                        tempEmptyCnt--;
+                    }
+
                 }
+
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "인지활동 (2)";
                 cell.classList.add('calendar__index');
+                tempEmptyCnt = emptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
+                    if(tempEmptyCnt==0 || tbCalendar.rows.length >= 7){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                    }else {
+                        tempEmptyCnt--;
+                    }
+
                 }
+
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "일상 생활 활동";
                 cell.classList.add('calendar__index');
+                tempEmptyCnt = emptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
+                    if(tempEmptyCnt==0 || tbCalendar.rows.length >= 7){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                    }else {
+                        tempEmptyCnt--;
+                    }
+
                 }
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
@@ -143,43 +210,141 @@ function buildCalendar(){//현재 달 달력 만들기
             }
         }
 
+        let lastEmptyCnt = 0; //달력 뒤 숫자 이후 공백 카운트하기
+
         while(true){
             if(cnt%7 != 0){
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.classList.add('calendar__number');
                 cnt = cnt + 1;//열의 갯수를 계속 다음으로 위치하게 해주는 역할
+                lastEmptyCnt++;
             }else{
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "인지활동 (1)";
                 cell.classList.add('calendar__index');
+                tempEmptyCnt = 7-lastEmptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
+                    if(tempEmptyCnt>0){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                        tempEmptyCnt--;
+                    }
                 }
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "인지활동 (2)";
                 cell.classList.add('calendar__index');
+                tempEmptyCnt = 7-lastEmptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
+                    if(tempEmptyCnt>0){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                        tempEmptyCnt--;
+                    }
                 }
                 row = calendar.insertRow();
                 cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
                 cell.innerHTML = "일상 생활 활동";
                 cell.classList.add('calendar__index');
+                tempEmptyCnt = 7-lastEmptyCnt;
+  
                 for (j=1; j<=7; j++){
                     cell = row.insertCell();//열 한칸한칸 계속 만들어주는 역할
-                    
+                    if(tempEmptyCnt>0){
+                        const select = document.createElement('select');
+    
+                        visual_problems.forEach(element => {
+                            const option = document.createElement("option");
+                            option.value = element;
+                            option.text = element;
+                            select.appendChild(option);
+                        });
+
+                        cell.appendChild(select);
+                        tempEmptyCnt--;
+                    }
                 }
                 break;
             }
         }
-
 }
 
-let visual_problems = ['같은그림찾기(하)', '같은도형찾기', '반쪽그림따라그리기', '특정글자찾기', '틀린그림찾기', '특정인물찾기', '같은글미잇기', '반쪽그림잇기', '이름찾아적기', '같은그림찾기(중)'];
-let concentration_problems = ['미로찾기', '사다리타기', '색칠하기', '기념일 색칠하기', '같은 숫자 연결하기', '화투 색칠하기', '글자색깔맞추기', '선따라 그리기', '따라그리기(중)', '따라그리기(하)'];
-let thinking_problems = ['물건개수 맞추기', '4x4스도쿠', '개수맞추기', '범주찾기', '주제와다른것찾기', '시장물건 계산하기', '주사위 계산하기', '열매개수세기', '숫자 나열하기'];
-let language_problems = ['빈칸채우기', '초성맞추기', '동물속담맞추기', '끝말잇기', '단어와 그림맞추기', '단어만들기', '명언따라쓰고읽기', '문장따라말하기'];
-let remembrance_problems = ['기념일 회상하기', '요리재료 맞추기', '식재료 구분하기', '옛물건 회상하기', '정보 기억하기', '음식 회상하기', '사물회상하기', '계절 회상하기', '옛놀이 회상하기'];
-let life_problems = ['시간 맞추기', '야외활동 함께하기', '실내활동 함께하기', '집안일 함께하기', '냉장고 식재료 찾기', '표정 맞추기', '실내운동 함께하기', '물건의 용도 알기'];
+// problem category button clicked
+const visual_btn = document.querySelector('.visual__btn');
+const concentration_btn = document.querySelector('.concentration__btn');
+const thinking_btn = document.querySelector('.thinking__btn');
+const language_btn = document.querySelector('.language__btn');
+const remembrance_btn = document.querySelector('.remembrance__btn');
+const life_btn = document.querySelector('.life__btn');
+
+make_problem_table(visual_problems);
+
+visual_btn.addEventListener('click', (event) => {
+    make_problem_table(visual_problems);
+});
+
+concentration_btn.addEventListener('click', (event) => {
+    make_problem_table(concentration_problems);
+});
+
+thinking_btn.addEventListener('click', (event) => {
+    make_problem_table(thinking_problems);
+});
+
+language_btn.addEventListener('click', (event) => {
+    make_problem_table(language_problems);
+});
+
+remembrance_btn.addEventListener('click', (event) => {
+    make_problem_table(remembrance_problems);
+});
+
+life_btn.addEventListener('click', (event) => {
+    make_problem_table(life_problems);
+});
+
+
+function make_problem_table(problems){
+
+    let cnt = 0;
+    const problems_table = document.querySelector('.problems__table');
+    while(problems_table.rows.length > 1)
+        problems_table.deleteRow(1);
+    while(true){
+        if(cnt==18)
+            break;
+
+        if(cnt%3 == 0)        
+            var row = problems_table.insertRow();
+        
+        cell = row.insertCell();
+        cell.classList.add('problems__td');
+        if(cnt<problems.length){
+            cell.innerHTML = problems[cnt];
+        }
+
+        cnt++;
+    }
+}
+
